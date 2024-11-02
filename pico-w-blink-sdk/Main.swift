964bc5068f9da8ffcda90a1815909a5cab2602aa
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-11-02 15:24:28 -0400

    Add `CYW43.AccessPoint`

--- pico-w-blink-sdk/Main.swift
+++ pico-w-blink-sdk/Main.swift
@@ -13,14 +13,26 @@
 struct Main {
 
     static func main() {
+
         let cyw43: CYW43
         do {
-            try cyw43 = CYW43()
+            cyw43 = try CYW43()
         }
         catch {
             print("Wi-Fi init failed.")
             return
         }
+
+        #if ACCESS_POINT
+        let accessPoint: CYW43.AccessPoint
+        do {
+            accessPoint = try CYW43.AccessPoint(ssid: "Pi Pico W", password: "1234")
+        }
+        catch {
+            print("Access Point init failed.")
+            return
+        }
+        #endif
         
         while true {
             cyw43.dot()
@@ -91,6 +103,60 @@ extension CYW43 {
 
         case vbus = 2
     }
+}
+
+extension CYW43 {
+
+    struct AccessPoint: ~Copyable {
+
+        /// Enables Wi-Fi AP (Access point) mode.
+        /// 
+        /// This enables the Wi-Fi in Access Point mode such that connections can be made to the device by other Wi-Fi clients.
+        init(
+            ssid: StaticString,
+            password: StaticString, 
+            authentication: WiFiAuthentication = .wpa2AESPSK
+        ) throws(PicoError) {
+            ssid.withUTF8Buffer { ssidBuffer in
+                password.withUTF8Buffer { passwordBuffer in
+                    cyw43_arch_enable_ap_mode(ssidBuffer.baseAddress, passwordBuffer.baseAddress, authentication.rawValue)
+                }
+            }
+        }
+
+        /// Enables Wi-Fi AP (Access point) mode.
+        /// 
+        /// This enables the Wi-Fi in Access Point mode such that connections can be made to the device by other Wi-Fi clients.
+        init(
+            ssid: StaticString
+        ) throws(PicoError) {
+            ssid.withUTF8Buffer { ssidBuffer in
+                cyw43_arch_enable_ap_mode(ssidBuffer.baseAddress, nil, UInt32(CYW43_AUTH_OPEN))
+            }
+        }
+
+        deinit {
+            cyw43_arch_disable_ap_mode()
+        }
+    }
+}
+
+enum WiFiAuthentication: UInt32, Copyable, Sendable {
+    
+    /// WPA authorization using TKIP (Temporal Key Integrity Protocol).
+    case wpaTKIPPSK = 0x00200002
+    
+    /// WPA2 authorization using AES (Advanced Encryption Standard); preferred option.
+    case wpa2AESPSK = 0x00400004
+    
+    /// Mixed WPA/WPA2 authorization for compatibility with both types.
+    case wpa2MixedPSK = 0x00400006
+    
+    /// WPA3 authorization using SAE (Simultaneous Authentication of Equals) with AES.
+    case wpa3SAE_AESPSK = 0x01000004
+    
+    /// Mixed WPA2/WPA3 authorization for compatibility with both WPA2 and WPA3 devices.
+    case wpa3WPA2_AESPSK = 0x01400004
 }
 
 enum PicoError: Int32, Error, Copyable, Sendable {
